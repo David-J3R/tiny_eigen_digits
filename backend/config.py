e1202c510be70b06309c78aed3af17d1fe98f895
David-J3R <181415601+David-J3R@users.noreply.github.com>
2025-10-18 15:22:32 +0200

    fixed issue with path in config.py, add CORS for localhost, and succesfully build Dockerfile

--- backend/config.py
+++ backend/config.py
@@ -23,7 +23,12 @@ class GlobalConfig(BaseConfig):
     )
 
     # CORS settings
-    CORS_ALLOWED_ORIGINS: List[str] = ["http://localhost:3000"]
+    CORS_ALLOWED_ORIGINS: List[str] = [
+        "http://localhost:3000",
+        "http://localhost:5500",
+        "http://localhost:8000",
+        "http://127.0.0.1:5500",  # LiveServer alternative
+    ]
 
     # API settings
     API_V1_PREFIX: str = "/api/v1"
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 from .config import config
 from .routers.cnn_predict import router as predict_router
@@ -15,15 +16,15 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=config.CORS_ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
 # Consistent Health Check Endpoint
-@app.get("/", tags=["Health Check"])
+@app.get("/", include_in_schema=False)
 def root():
-    return {"status": "ok", "message": f"{config.APP_NAME} is running."}
+    return FileResponse("frontend/index.html")
 
 
 # Include Routers
