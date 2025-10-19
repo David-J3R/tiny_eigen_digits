76b870b8aec2119de4720baf8d12be6388616ef4
David-J3R <181415601+David-J3R@users.noreply.github.com>
2025-10-19 11:15:11 +0200

    Add github CORS

--- backend/config.py
+++ backend/config.py
@@ -27,7 +27,7 @@ class GlobalConfig(BaseConfig):
         "http://localhost:3000",
         "http://localhost:5500",
         "http://localhost:8000",
-        "http://127.0.0.1:8000",
+        "http://127.0.0.1:8000/api/v1/predict",
         "http://127.0.0.1:5500",  # LiveServer alternative
     ]
 
