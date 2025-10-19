f42b43172d583f52d369382b4d39e83dead13007
David-J3R <181415601+David-J3R@users.noreply.github.com>
2025-10-19 11:14:03 +0200

    Add allowed origins

--- backend/config.py
+++ backend/config.py
@@ -27,8 +27,8 @@ class GlobalConfig(BaseConfig):
         "http://localhost:3000",
         "http://localhost:5500",
         "http://localhost:8000",
+        "http://127.0.0.1:8000",
         "http://127.0.0.1:5500",  # LiveServer alternative
-        "https://david-gl3z.com",  # Github Pages domain
     ]
 
     # API settings
